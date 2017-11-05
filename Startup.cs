e281da0755d78893c4b526a860eb9303de5e346a
BuildTools <unconfigured@null.spigotmc.org>
2017-11-05 14:42:40 +0100

    Test
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace ContinuousDeployment
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
